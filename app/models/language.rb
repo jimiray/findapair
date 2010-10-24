38febef432eac5ea8b9a88fc12165535d5076ae7
Jim Ray <jim@mosaicglobe.com>
2010-10-24 07:28:10 -0600

    rename and add devise

---
+++ app/models/language.rb
@@ -0,0 +1,5 @@
+class Language < ActiveRecord::Base
+  
+  validates_presence_of :name
+
+end
---
+++ db/migrate/20101024122201_create_languages.rb
@@ -0,0 +1,12 @@
+class CreateLanguages < ActiveRecord::Migration
+  def self.up
+    create_table :languages do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :languages
+  end
+end

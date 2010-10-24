ef436e8b9025421edb901e518a724168f21062ea
Jim Ray <jim@mosaicglobe.com>
2010-10-24 05:55:23 -0600

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindapairMe::Application.config.secret_token = 'cf66f2207eb17992b07c7dcb43d92fbc2c2e447c5699293397ce2932e6c39f360bc69f56e8e6e323b181509d2630cee6e696f50554f24db0ba0ca044739f3773'

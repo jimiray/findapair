c35299dae807251e6fb04a8ff33256862db65498
jim <jimiray@gmail.com>
2011-02-08 14:00:14 -0700

    ensure username unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :twitter, :github, :name, :city
 
+  validates_uniqueness_of :username
+
   [:twitter, :github].each do |c|
     validates_length_of c, :maximum => 64
   end

ba5941eb5b17b07c3d836c281e2dc936bde00a11
Jim Ray <jimiray@gmail.com>
2012-01-24 15:42:39 -0700

    updates for styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
+
   def show
     @user = User.find_by_username(params[:id])
     raise ActiveRecord::RecordNotFound unless @user
